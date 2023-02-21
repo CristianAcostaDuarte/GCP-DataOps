7a5d94b434f247cb75fbfe4e8f981327ae5ed7cf
CristianAcostaDuarte <101611537+CristianAcostaDuarte@users.noreply.github.com>
2023-02-21 23:51:10 +0000

    Editing the files

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM alpine:latest
 RUN apk update && apk add bash
 
 WORKDIR /app
-COPY repeat.sh /app
--- test_main.py
+++ test_main.py
@@ -1,9 +1,3 @@
 """
 Test goes here
 """
-
-from mylib.calculator import add
-
-
-def test_add():
-    assert add(1, 2) == 3
